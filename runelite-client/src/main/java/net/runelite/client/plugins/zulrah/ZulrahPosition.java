6e0424ba21c945fcc9b8d6f61e0aede73ce7d2ce
Ajax Gunterson <nolan.ripplinger@gmail.com>
2019-06-03 21:16:37 -0500

    Added infrastructure (fully) to help determine which location zulrah has spawned.
    Now to find a good way to determine which pattern is taken, then put down ground markers.

--- runelite-client/src/main/java/net/runelite/client/plugins/zulrah/ZulrahPosition.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/zulrah/ZulrahPosition.java
@@ -2,18 +2,18 @@ package net.runelite.client.plugins.zulrah;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import net.runelite.api.coords.WorldPoint;
-
+import net.runelite.api.coords.LocalPoint;
 
 @Getter
 @RequiredArgsConstructor
 public enum ZulrahPosition {
 
-        CENTER(new WorldPoint(11492,6243,0)),
-        EAST(new WorldPoint(10158,7201,0)),
-        SOUTH(new WorldPoint(8996,7576,0)),
-        WEST(new WorldPoint(8986,7585,0));
+        CENTER(new LocalPoint(6720,7616)),
+        EAST(new LocalPoint(8000,7360)),
+        SOUTH(new LocalPoint(6720,6208)),
+        WEST(new LocalPoint(5440,7360));
 
-        private final WorldPoint position;
+
+        private final LocalPoint position;
 
 }
